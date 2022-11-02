782679d9f155d4ab44384920637f4c6522f7a771
andriGo <afomin129@gmail.com>
2022-11-03 01:21:36 +0200

    totalVariables

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function priceOfProducts(saltBeefPrice, saltBeefRequest, riceRequest) {
-	// write your code here
+	const ricePrice = Math.round(saltBeefPrice / 3 * 100) / 100;
+	const totalRicePrice = riceRequest * ricePrice;
+	const totalSaltBeefPrice = saltBeefPrice * saltBeefRequest;
+	return totalRicePrice + totalSaltBeefPrice;
+
 }
 
 module.exports = priceOfProducts;
